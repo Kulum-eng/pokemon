d41d4440c42ed1dfdff490e6f9a2aa4218334cb6
Kulum-eng <120421270+Kulum-eng@users.noreply.github.com>
2024-12-03 13:38:31 -0600

    f

--- src/app/poke-api.service.spec.ts
+++ src/app/poke-api.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { PokeApiService } from './pages/poke-api.service';
+import { PokeApiService } from '../app/pages/services/poke-api.service'
 
 describe('PokeApiService', () => {
   let service: PokeApiService;
